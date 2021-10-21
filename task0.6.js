214beb325327b6f066e264096f489ef7b7997f3f
Buckweed2020 <b.chokoe@gmail.com>
2021-10-21 21:00:09 +0200

    accounted for not defined error

--- task0.6.js
+++ task0.6.js
@@ -5,8 +5,14 @@ function findMax(number1,number2,number3) {
         return number2
     } else if(number3 > number1 && number3 > number2 ) {
         return number3
+    } else if(number1 == number2 || number1 == number3 || number2 == number3) {
+        return number1
+    } else if(number2 > number1 && number2 == number3) {
+        return number2
+    } else if(number1 > number2 && number1 == number3) {
+        return number1
     }
 }
 
-findMax(10,20,21)
+findMax(21,21,21)
 
