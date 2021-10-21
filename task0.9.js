f4a0ca2f3b8894c7fdbbd908bf69e3719dca23c7
Buckweed2020 <b.chokoe@gmail.com>
2021-10-21 12:04:42 +0200

    error fixed, now returns vowels

--- task0.9.js
+++ task0.9.js
@@ -1,5 +1,9 @@
 function returnVowels(word) {
     word = word.replace( /[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/g, '' );
-}
+
+    Array.from(word);
+
+    return Array.from(word);
+}  
 
 console.log("Vowels: " + returnVowels("Rinnegan"))
