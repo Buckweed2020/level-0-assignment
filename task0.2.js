e21a274bb02a9626b77d5d75f5faf484f99524cd
Buckweed2020 <b.chokoe@gmail.com>
2021-10-21 20:55:00 +0200

    declared variables properly

--- task0.2.js
+++ task0.2.js
@@ -1,6 +1,6 @@
-number1 = 1 + 1 * 2;
-number2 = (1 + 1) * 2;
-number3 = 1 + 1 * 2 / 2;
-number4 = (1 + 1 * 2 ) / 2;
+let number1 = 1 + 1 * 2;
+let number2 = (1 + 1) * 2;
+let number3 = 1 + 1 * 2 / 2;
+let number4 = (1 + 1 * 2 ) / 2;
 
 console.log(number1,number2,number3,number4);
