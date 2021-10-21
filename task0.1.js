31d4d87238681a4efa706880aff03dc01e0c2cdf
Buckweed2020 <b.chokoe@gmail.com>
2021-10-21 20:53:39 +0200

    declared variables properly

--- task0.1.js
+++ task0.1.js
@@ -1,11 +1,11 @@
-number1 = 0;
-number2 = 1;
+let number1 = 0;
+let number2 = 1;
 
 console.log(number1);
 console.log(number2);
 
 number1 = number1 + 3;
-y = y + x;
+number2 = number2 + number1;
 
 console.log(number1);
-console.log(numer1);
+console.log(number1);
