9b7aa88f628360dd47463df0124acf4dd3cc48c8
Buckweed2020 <b.chokoe@gmail.com>
2021-10-21 20:57:30 +0200

    used print instead of return. fixed indentation

--- task0.4.js
+++ task0.4.js
@@ -1,8 +1,10 @@
 function evenOrOdd(value) {
-    if (value % 2 == 0)
-    return "even";
-     else 
-         return "odd";
+    if (value % 2 == 0) {
+        return "even"
+    }
+    else {
+        return "odd"
+    }
 }
 
-evenOrOdd(8)
+console.log(evenOrOdd(8))
