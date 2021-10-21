a83ff765144778d27df3cb5ddb03e3a821a89f07
Buckweed2020 <b.chokoe@gmail.com>
2021-10-21 12:14:55 +0200

    function now returns vowels instead of error

--- task0.9.js
+++ task0.9.js
@@ -2,8 +2,6 @@ function returnVowels(word) {
     word = word.replace( /[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/g, '' );
 
     Array.from(word);
-
-    return Array.from(word);
 }  
 
-console.log("Vowels: " + returnVowels("Rinnegan"))
+console.log("Vowels: " + returnVowels("Rinnegan"));
