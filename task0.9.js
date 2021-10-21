3fefe16785da3de6b2c116d655e9c36869314ce9
Buckweed2020 <b.chokoe@gmail.com>
2021-10-21 17:04:21 +0200

    function now shows vowels

--- task0.9.js
+++ task0.9.js
@@ -2,6 +2,7 @@ function returnVowels(word) {
     word = word.replace( /[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/g, '' );
 
     Array.from(word);
+    return word.split("")
 }  
 
 console.log("Vowels: " + returnVowels("Rinnegan"));
